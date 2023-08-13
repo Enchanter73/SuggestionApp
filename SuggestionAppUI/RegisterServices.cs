a06c826a22626a4d15a3a8e439545a49a37fc37e
Selim <Selimcesur73@gmail.com>
2023-08-13 19:09:32 +0300

    Added and Rearrenged some files

---
+++ SuggestionAppUI/RegisterServices.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SuggestionAppUI
+{
+    public static class RegisterServices
+    {
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddMemoryCache();
+        }
+    }
+}

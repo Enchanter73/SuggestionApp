7d033acdbff7a60d733170d91989d3e315a3b7ef
Selim <Selimcesur73@gmail.com>
2023-08-18 23:24:54 +0300

    DI added for data access classes

--- SuggestionAppUI/RegisterServices.cs
+++ SuggestionAppUI/RegisterServices.cs
@@ -9,6 +9,13 @@ namespace SuggestionAppUI
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddMemoryCache();
+
+            builder.Services.AddSingleton<IDbConnection, DbConnection>();
+
+            builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
+            builder.Services.AddSingleton<IStatusData, MongoStatusData>();
+            builder.Services.AddSingleton<IUserData, MongoUserData>();
+            builder.Services.AddSingleton<ISuggestionData, MongoSuggestionData>();            
         }
     }
 }
